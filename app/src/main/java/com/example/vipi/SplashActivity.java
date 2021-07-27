6334abfc00c8d1cfca2256a527df3a1b007c2a95
Yurii Soliar <us1585707@gmail.com>
2021-07-27 23:16:35 +0300

    VIPI Splash Activity

---
+++ app/src/main/java/com/example/vipi/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.vipi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView textV, textI, textP, textI2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        textV = findViewById(R.id.textV);
+        textI = findViewById(R.id.textI);
+        textP = findViewById(R.id.textP);
+        textI2 = findViewById(R.id.textI2);
+        textV.animate().translationY(-400).setDuration(2000);
+        textI.animate().translationX(400).setDuration(2000);
+        textP.animate().translationX(-400).setDuration(2000);
+        textI2.animate().translationY(400).setDuration(2000);
+
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2200);
+    }
+}
